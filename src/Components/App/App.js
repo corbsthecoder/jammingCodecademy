b457016d84181ff58c84d92bb8ae954e3b0f662d
corbsthecoder <77516035+corbsthecoder@users.noreply.github.com>
2021-05-29 11:44:55 +1200

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,86 @@
+import './App.css';
+import React from 'react';
+import {SearchBar} from '../SearchBar/SearchBar';
+import {SearchResults} from '../SearchResults/SearchResults';
+import {Playlist} from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],  //This will be the results on the left when a search is made
+      playlistName: 'Cool Playlist', //Playlist name - although this is not currently used
+      playlistTracks: [],  //Playlist track to be added to Spotify
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  //Add track from left to right. Checks the ID to make sure that it is different from the right. If it is the same it will not add
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  //Removes track from right side. Filters through state's array, retains array objects that do not match track.id
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({playlistTracks: tracks});
+  }
+
+  //Change playlist name - function linked to Playlist component
+  updatePlaylistName(name) {
+    this.setState({playlistName: name });
+  }
+
+  //This will be a method used to save the playlist to the users spotify account.
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({ 
+        playlistName: 'New Playlist',
+        playlistTracks: [], 
+      });
+    })
+  }
+
+  //search term used to hook up to the spotify API
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar onSearch={this.search}/>
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} 
+                      playlistTracks={this.state.playlistTracks} 
+                      onRemove={this.removeTrack} 
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
